f9ceb37b08acf0f4bc249346706abe852f0eabba
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-05-18 08:03:30 -0700

    Tweek alum table

--- app/models/alum.rb
+++ app/models/alum.rb
@@ -2,7 +2,7 @@ class Alum < ActiveRecord::Base
 
   belongs_to :bootcamp
   
-  validates :name, :company, :compurl, :city, :state, :linkurl, :title, :bootcamp_id, presence: true
+  validates :name, :company, :compurl, :city, :state, :linkurl, :bootcamp_id, presence: true
 
 
 end
